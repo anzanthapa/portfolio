aa1fae1f83dccdcef3d689301a5033d86c3843b7
Bhoj Raj Thapa <anzanthapasocial@gmail.com>
2025-07-12 15:23:34 -0400

    added SLC school

--- assets/js/main.js
+++ assets/js/main.js
@@ -277,4 +277,11 @@
     });
   });
 
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a[href^="http"]').forEach(a => {
+      a.setAttribute('target', '_blank');
+      a.setAttribute('rel', 'noopener noreferrer');
+    });
+  });
+
 })()
